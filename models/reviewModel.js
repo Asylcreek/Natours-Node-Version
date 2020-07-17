6d22e662f99f2f49a4fd5df3822041ee203f34a4
Boro Omokugbo Joseph <omokugbobr@gmail.com>
2020-07-17 21:14:47 +0100

    Implement updating of ratingsAverage and ratingsQuantity, and implement preventing duplicate reviews

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Tour = require('./tourModel');
 
 const reviewSchema = new mongoose.Schema({
     review: { type: String, required: [true, 'Review cannot be empty'] },
@@ -23,6 +24,10 @@ const reviewSchema = new mongoose.Schema({
     },
 });
 
+//Indexes
+reviewSchema.index({ tour: 1, user: 1 }, { unique: true });
+
+//Document middleware
 reviewSchema.pre(/^find/, function(next) {
     const populateQuery = [
         { path: 'user', select: 'name photo' },
@@ -32,6 +37,47 @@ reviewSchema.pre(/^find/, function(next) {
     this.populate(populateQuery);
 
     next();
+});
+
+reviewSchema.statics.calcAverageRating = async function(tourId) {
+    const stats = await this.aggregate([{
+            $match: { tour: tourId },
+        },
+        {
+            $group: {
+                _id: '$tour',
+                numOfRatings: { $sum: 1 },
+                avgRating: { $avg: '$rating' },
+            },
+        },
+    ]);
+
+    if (stats.length > 0) {
+        await Tour.findByIdAndUpdate(tourId, {
+            ratingsQuantity: stats[0].numOfRatings,
+            ratingsAverage: stats[0].avgRating,
+        });
+    } else {
+        await Tour.findByIdAndUpdate(tourId, {
+            ratingsQuantity: 0,
+            ratingsAverage: 4.5,
+        });
+    }
+};
+
+reviewSchema.post('save', function() {
+    //this.constructor point to the model
+    this.constructor.calcAverageRating(this.tour);
+});
+
+reviewSchema.pre(/^findOneAnd/, async function(next) {
+    this.r = await this.findOne();
+
+    next();
+});
+
+reviewSchema.post(/^findOneAnd/, async function() {
+    await this.r.constructor.calcAverageRating(this.r.tour);
 });
 
 const Review = mongoose.model('Review', reviewSchema);
