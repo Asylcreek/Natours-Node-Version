df6f20cee5e1a43f14b99750a02a72e30750328a
Boro Omokugbo Joseph <omokugbobr@gmail.com>
2020-07-20 21:18:01 +0100

    Build overview, tour, and login pages and create the view routes and route handlers

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,7 +1,26 @@
-exports.getOverview = (req, res) => {
-    res.status(200).render('overview', { title: 'All Tours' });
-};
+const Tour = require('../models/tourModel');
+const catchAsync = require('../Utils/catchAsync');
 
-exports.getTour = (req, res) => {
-    res.status(200).render('tour', { title: 'The Forest Hiker' });
+exports.getOverview = catchAsync(async(req, res, next) => {
+    //Get Tour data from collection
+    const tours = await Tour.find();
+
+    //Render Template
+    res.status(200).render('overview', { title: 'All Tours', tours });
+});
+
+exports.getTour = catchAsync(async(req, res, next) => {
+    const { slug } = req.params;
+
+    const tour = await Tour.findOne({ slug }).populate({
+        path: 'reviews',
+        select: 'review rating user -tour',
+    });
+
+    //Render Template
+    res.status(200).render('tour', { tour, title: `${tour.name} Tour` });
+});
+
+exports.getLoginForm = (req, res) => {
+    res.status(200).render('login', { title: 'Log into your account' });
 };
